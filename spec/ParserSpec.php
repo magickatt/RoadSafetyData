b03c0a5008b58c2d77d4dff680b304f5df55fc27
Andrew Kirkpatrick <development@andrew-kirkpatrick.com>
2015-01-02 17:06:36 +0000

    Created initial spec

---
+++ spec/ParserSpec.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ParserSpec extends ObjectBehavior
+{
+    private $parser;
+
+    function let()
+    {
+
+    }
+
+    function it_should_be_able_to_parse_a_csv_file_and_return_rows_of_data()
+    {
+
+    }
+}
